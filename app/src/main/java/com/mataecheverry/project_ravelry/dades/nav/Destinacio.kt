30fde361d5815966c21d0a1513077aad29e09888
fiona <a2106892@institutmontilivi.cat>
2024-04-09 18:35:40 +0200

    Commit amb nous colors i Classe Destinacio.kt acabada. Afegit també icones de Flaticon

---
+++ app/src/main/java/com/mataecheverry/project_ravelry/dades/nav/Destinacio.kt
@@ -0,0 +1,84 @@
+package com.mataecheverry.project_ravelry.dades.nav
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.mataecheverry.project_ravelry.R
+
+
+enum class NavigationCat(
+    val previousPath: String,
+    @DrawableRes val icon: Int,
+    @StringRes val title: Int
+){
+    Login ("Login", R.drawable.user, R.string.login),
+    Register("Registre", R.drawable.user, R.string.register),
+    Home("Home", R.drawable.home, R.string.home),
+    Search("Search", R.drawable.search, R.string.search),
+    Patterns("Patterns", R.drawable.fakeyarn, R.string.patterns),
+    Favorites("Favorites", R.drawable.heart, R.string.favorites),
+    Projects("Projects", R.drawable.projects, R.string.projects),
+    Shops("Shops", R.drawable.shop, R.string.shop),
+    UploadProject("UploadProject", R.drawable.plus, R.string.uploadproject),
+    Calendar("Calendar", R.drawable.calendar, R.string.calendar),
+    About("About", R.drawable.about, R.string.about)
+}
+
+
+sealed class Destinacio(
+    val basePath: String,
+    val navArguments: List<NavArguments> = emptyList()
+){
+
+    val navArgs = navArguments.map { it.toNavArgument() }
+    val genericPath = run {
+        val argumentKeys = navArguments.map {"{${it.key}}"}
+        listOf(basePath)
+            .plus(argumentKeys)
+            .joinToString("/")
+    }
+
+    object Login: Destinacio(NavigationCat.Home.previousPath+"/Start")
+    object Register: Destinacio(NavigationCat.Search.previousPath+"/Register")
+    object Recover: Destinacio(NavigationCat.Search.previousPath+"/Recover")
+    object Home: Destinacio(NavigationCat.Home.previousPath+"/Start")
+    object Search: Destinacio(NavigationCat.Search.previousPath+"/Start")
+    object Patterns: Destinacio(NavigationCat.Patterns.previousPath+"/Start")
+    object PatternDetails: Destinacio(NavigationCat.Patterns.previousPath+"/Patterns",
+            listOf(NavArguments.IdDetail)){
+        fun CreatePathWithArguments(IdDetail: String) = "$basePath/$IdDetail"
+    }
+    object Favorites: Destinacio(NavigationCat.Favorites.previousPath+"/Start")
+    object FavoriteDetails: Destinacio(NavigationCat.Favorites.previousPath+"/Favorites")
+    object Projects: Destinacio(NavigationCat.Favorites.previousPath+"/Start")
+    object ProjectDetails: Destinacio(NavigationCat.Projects.previousPath+"/Projects",
+        listOf(NavArguments.IdDetail)){
+        fun CreatePathWithArguments(IdDetail: String) = "$basePath/$IdDetail"
+    }
+
+    object Shops: Destinacio(NavigationCat.Shops.previousPath+"/Start")
+    object ShopDetails: Destinacio(NavigationCat.Shops.previousPath+"/Shops",
+        listOf(NavArguments.IdDetail)){
+        fun CreatePathWithArguments(IdDetail: String) = "$basePath/$IdDetail"
+    }
+
+    object Calendar: Destinacio(NavigationCat.Calendar.previousPath+"/Start")
+    object CalendarDetails: Destinacio(NavigationCat.Calendar.previousPath+"/Calendar",
+        listOf(NavArguments.IdDetail)){
+        fun CreatePathWithArguments(IdDetail: String) = "$basePath/$IdDetail"
+    }
+    object UploadProject: Destinacio(NavigationCat.UploadProject.previousPath+"/Start")
+    object About: Destinacio(NavigationCat.About.previousPath+"/Start")
+
+}
+
+enum class NavArguments(val key: String, var type: NavType<*>){
+    IdDetail("IdDetail", NavType.StringType);
+
+    fun toNavArgument(): NamedNavArgument{
+        return navArgument(key) {type}
+    }
+
+}
--- app/src/main/java/com/mataecheverry/project_ravelry/ui/Aplicacio.kt
+++ app/src/main/java/com/mataecheverry/project_ravelry/ui/Aplicacio.kt
@@ -1,28 +1,33 @@
 package com.mataecheverry.project_ravelry.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -30,14 +35,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.mataecheverry.project_ravelry.ui.theme.Project_RavelryTheme
+import coil.compose.AsyncImage
+import com.mataecheverry.project_ravelry.dades.app_models.AppUser
+import com.mataecheverry.project_ravelry.dades.nav.Destinacio
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+
+val displaysWithouthDrawer = listOf(
+    Destinacio.Login.genericPath,
+    Destinacio.Register.genericPath,
+    Destinacio.Recover.genericPath
+)
+
+val displaysWithDrawer = listOf(
+    Destinacio.Home.genericPath,
+    Destinacio.Search.genericPath,
+    Destinacio.Patterns.genericPath,
+    Destinacio.PatternDetails.genericPath,
+    Destinacio.Favorites.genericPath,
+    Destinacio.FavoriteDetails.genericPath,
+    Destinacio.Projects.genericPath,
+    Destinacio.ProjectDetails.genericPath,
+    Destinacio.UploadProject.genericPath,
+    Destinacio.Shops.genericPath,
+    Destinacio.ShopDetails.genericPath,
+    Destinacio.Calendar.genericPath,
+    Destinacio.CalendarDetails.genericPath,
+    Destinacio.About.genericPath
+)
+
 
 @Preview
 @Composable
@@ -54,7 +87,6 @@ fun Aplicacio(){
 
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RavelryScaffold(
@@ -64,7 +96,6 @@ fun RavelryScaffold(
     rutaActual: String,
     snackbarHostState: SnackbarHostState
 ){
-
     val topAppBarTitle = ""
     /**Posar TITOLS ADEQUATS A TRAVÉS DE RUTES*/
 
@@ -75,55 +106,51 @@ fun RavelryScaffold(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary)
-            )
-        },
-//        navigationIcon = {
-//            if (rutaActual in listOf(
-//                    Destinacio.Portada.rutaGenerica,
-//                    Destinacio.Fotografies.rutaGenerica,
-//                    Destinacio.Preferencies.rutaGenerica,
-//                    Destinacio.QuantA.rutaGenerica,
-//                    Destinacio.Preferits.rutaGenerica
-//                )
-//            //Destinacio.Partida.rutaGenerica,
-//            ) {
-//
-//                IconButton(onClick = {
-//                    coroutineScope.launch {
-//                        if (estatDrawer.isClosed)
-//                            estatDrawer.open()
-//                        else
-//                            estatDrawer.close()
-//                    }
-//                }) {
-//
-//                    Icon(
-//                        imageVector = Icons.Default.Menu,
-//                        contentDescription = null,
-//                        tint = MaterialTheme.colorScheme.onPrimary
-//                    )
-//                }
-//            } else {
-//                IconButton(onClick = { controladorDeNavegacio.navigateUp() }) {
-//
-//                    Icon(
-//                        imageVector = Icons.Rounded.ArrowBack,
-//                        contentDescription = null,
-//                        tint = MaterialTheme.colorScheme.onPrimary
-//                    )
-//                }
-//            }
-//      }
-        bottomBar = {
-                    BottomAppBar {
-
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary),
+                navigationIcon = {
+                    if (rutaActual in displaysWithDrawer)
+                    {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                if (estatDrawer.isClosed)
+                                    estatDrawer.open()
+                                else
+                                    estatDrawer.close()
+                            }
+                        })
+                        {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    } else {
+                        IconButton(onClick = { controladorDeNavegacio.navigateUp() }) {
+                            Icon(
+                                imageVector = Icons.Rounded.ArrowBack,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
                     }
+                }
+            ) },
+        bottomBar = {
+            BottomAppBar (
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ){Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "AQUI BOTONS LUPA I +")
+            }
         },
-
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
         },
+        //De normal ha d'estar amagat
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 text = {Text("Snaki snsack snack")},
@@ -149,14 +176,10 @@ fun RavelryScaffold(
                     }
                 }
             )
-        },
-
-
+        }
     ) {
         Modifier.padding(it)
     }
-
-
 }
 
 
@@ -166,8 +189,67 @@ fun RavelryDrawer(
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
     estatDrawer: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
     modifier: Modifier,
-    rutaActual: String
+    rutaActual: String,
+    appUser: AppUser
 
 ){
+    ModalNavigationDrawer(
+        modifier = modifier,
+        drawerState = estatDrawer,
+        drawerContent = {
+            ModalDrawerSheet (
+                drawerShape = ShapeDefaults.Small, //fa referència a la mida del corner radius
+                drawerContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                drawerContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                drawerTonalElevation = 5.dp,
+                windowInsets = WindowInsets(left = 24.dp, right = 24.dp, top = 48.dp) // És el padding
+            ){
+                AsyncImage (
+                    //Aqui ha d'anar la imatge d'usuari. tirarem d'un async de moment
+                   model = appUser.large_photo_url,
+                    contentDescription = "User selected profile picture.",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth)
+                Spacer (Modifier.height(48.dp))
+                HorizontalDivider(color = MaterialTheme.colorScheme.onSecondaryContainer,modifier= Modifier.height(15.dp))
+                Spacer (Modifier.height(48.dp))
+//                CategoriaDeNavegacioApp.entries.forEach {
+//                    NavigationDrawerItem  (
+//                        label = { Text(it.titol) },
+//                        selected = rutaActual.contains(it.rutaPrevia),
+//                        icon = {Icon (imageVector = it.icona, it.titol)},
+//                        onClick = {
+//                            coroutineScope.launch {
+//                                estatDrawer.close()
+//                            }
+//                            controladorDeNavegacio.navigate(it.rutaPrevia) {
+//                                popUpTo(controladorDeNavegacio.graph.findStartDestination().id){
+//                                    //guarda l'estat de la pantalla de la que marxem (funciona d'aquella manera,
+//                                    // No tots els valors es guarden))
+//                                    saveState = true
+//                                }
+//                                launchSingleTop = true
+//                                //Restaura l'estat de la pantalla i la deixa tal i com estava quan vam navegar a un altre lloc
+//                                restoreState = true
+//                            }  },
+//                        colors = NavigationDrawerItemDefaults.colors(
+//                            unselectedBadgeColor = MaterialTheme.colorScheme.error,
+//                            unselectedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+//                            unselectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+//                            unselectedTextColor = MaterialTheme.colorScheme.onSecondaryContainer,
+//                            selectedBadgeColor = MaterialTheme.colorScheme.error,
+//                            selectedContainerColor = MaterialTheme.colorScheme.tertiaryContainer,
+//                            selectedIconColor = MaterialTheme.colorScheme.onTertiaryContainer,
+//                            selectedTextColor = MaterialTheme.colorScheme.onTertiaryContainer),
+//                        //badge = {Icon(imageVector = Icons.Default.ArrowBack, null)},
+//                        shape = ShapeDefaults.Medium
+//                    )
+//                }
+            }
+        },
+        //gesturesEnabled = rutaActual in pantallesAmbDrawer
+    ) {
+       // GrafDeNavegacio(controladorDeNavegacio)
+    }
 
 }
