0a43c409bfa643a151914a83df33c9a8bb049381
Fiona Mata <98351983+FionaMata@users.noreply.github.com>
2024-06-11 15:59:37 +0200

    Constants.kt Hidden

--- app/src/main/java/com/mataecheverry/project_ravelry/dades/autenticacio/Constants.kt
+++ app/src/main/java/com/mataecheverry/project_ravelry/dades/autenticacio/Constants.kt
@@ -1,15 +1,15 @@
 package com.mataecheverry.project_ravelry.dades.autenticacio
 
 
-const val CLIENT_ID = "4f5e0bb2a42f4e9adee9d4a67aaa6295"
-const val CLIENT_SECRET = "HMZfZ1Nf8PLnA0vjYLg/FaqFTd7U6VkC9N2pUJRc"
+const val CLIENT_ID = 
+const val CLIENT_SECRET = 
 const val BASE_URL = "https://www.ravelry.com/"
-const val LOGIN_URL = "https://www.ravelry.com/oauth2/auth/"  //OAUTH2/AUTH
+const val LOGIN_URL = "https://www.ravelry.com/oauth2/auth/"  
 
 
 
 
-const val CALLBACK = "https://ravelschool-bc44d.firebaseapp.com/__/auth/handler"
+const val CALLBACK = 
 const val URL_TOKEN = "https://www.ravelry.com/oauth2/token/"
 
 
